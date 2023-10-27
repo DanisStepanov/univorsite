811bb55c942ec9f4b4096f1b3b30ea1775d3d8d9
PC <grezle2018@mail.ru>
2023-10-27 10:56:47 +0300

    27.10/2023

--- Extra_tasks/Dcalc/viewier.py
+++ Extra_tasks/Dcalc/viewier.py
@@ -9,3 +9,4 @@ def create_list_degrees(*args, **kwargs):
         lst.append(f"{k} = {dcalc.deg_to_gms(v)}")
     return lst
 print(create_list_degrees(172.25899161, 321.42304971, 12.697987681352, pole=21.89617856, put_1=140.85706440))
+print(__name__)
